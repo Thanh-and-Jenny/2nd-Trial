c9f4946e1937697ad95ef9e281090ad7ac0345eb
Jennieng0909 <114229056+Jennieng0909@users.noreply.github.com>
2022-09-23 23:09:02 +0700

    van chua hieu how it works

---
+++ dulieuchuoi2.py
@@ -0,0 +1,30 @@
+a = "Hello, December"
+b = "Have a great day\n"
+c= a+b
+d= "D"
+print(c)
+print(d in a)
+print(d in b)
+print(b*10)
+e= a[4]
+print(e )
+f = a[-1]
+print(f )
+a = "Hello, December"
+b = a[5:len(a ):]
+print(b)
+b = a[None: 5 ]
+print(b )
+c = a [None: None ]
+print( c )
+d = a[7: None ]
+print(d )
+a= 'Năm nay bạn %a tuổi' %('10 ')
+print(a )
+a = "Chúng ta có khoảng %a  người đến %a  người " %('10', "20 ")
+print(a ) 
+a= 'Họ khoảng %r  tuổi đến %r  tuổi ' 
+print(a %("25 ", "55"))
+a = "Họ có %s USD đến %s USD  " 
+r= a %('1000', '5000')
+print(r)
